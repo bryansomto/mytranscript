b46da4ef7450f79ecd438488319bef87a1180ce7
bryansomto <71612998+bryansomto@users.noreply.github.com>
2021-11-26 14:09:30 +0100

    Update Transcript.sol

--- contracts/Transcript.sol
+++ contracts/Transcript.sol
@@ -56,43 +56,4 @@ contract Transcript {
 		return (results[_matricNo].matricNo, results[_matricNo].examName, results[_matricNo].examUnit, results[_matricNo].examGrade);
 		emit TranscriptRequested(transcriptCount, _matricNo, _examName, _examUnit, _examGrade);
 	}
-
-	// function createProduct(string memory _name, uint _price) public {
-	// 	// Require a valid name
-	// 	require(bytes(_name).length > 0);
-	// 	// Require a valid price
-	// 	require(_price > 0);
-	// 	// Make sure parameters are correct
-	// 	// Increment product count
-	// 	productCount ++;
-	// 	// Create the product
-	// 	products[productCount] = Product(productCount, _name, _price, msg.sender, false);
-	// 	// Trigger an event
-	// 	emit ProductCreated(productCount, _name, _price, msg.sender, false);
-	// }
-
-	// function purchaseProduct(uint _id) public payable {
-	// 	// Fetch the product
-	// 	Product memory _product = products[_id];
-	// 	// Fetch the owner
-	// 	address payable _seller = _product.owner;
-	// 	// Make sure the product has valid id
-	// 	require(_product.id > 0 && _product.id <= productCount);
-	// 	// Require that there is enough ether in the transaction
-	// 	require(msg.value >= _product.price);
-	// 	// Require that the product has not been purchased already
-	// 	require(!_product.purchased);
-	// 	//require the buyer is not the seller
-	// 	require(_seller != msg.sender);
-	// 	// Transfer ownership to the buyer
-	// 	_product.owner = msg.sender;
-	// 	// Mark as purchased
-	// 	_product.purchased = true;
-	// 	// Update the product
-	// 	products[_id] = _product;
-	// 	// Pay the seller by sending them Ether
-	// 	address(_seller).transfer(msg.value);
-	// 	// Trigger an event
-	// 	emit ProductPurchased(productCount, _product.name, _product.price, msg.sender, true);
-	// }
 }
