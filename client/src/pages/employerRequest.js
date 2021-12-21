a8008c641a868f80482d6d3ccd39760aea19c927
Somtochukwu Ike-Adinnu <bryansomto@gmail.com>
2021-12-21 18:50:18 +0100

    ...

--- client/src/pages/employerRequest.js
+++ client/src/pages/employerRequest.js
@@ -5,11 +5,22 @@ import { Form, Button, Row, Col } from 'react-bootstrap';
 import { BrowserRouter as Router, } from "react-router-dom";
 
 class EmployerRequest extends Component {
+
+    viewRequest = () => {
+        const target = document.getElementById('tableArea');
+        var table = document.createElement('table');
+        table.setAttribute("class", "table table-striped text-center");
+
+        
+    }
+
     render() {
         return (
             <body>
                 <Student />
-                
+                <section id="tableArea">
+
+                </section>
             </body>
         )
     }
