f1b6d199d8ddfcf1c9423b3cfcb5ff2f27e5b291
Somtochukwu Ike-Adinnu <bryansomto@gmail.com>
2022-01-30 01:23:39 +0100

    Updates

---
+++ precache-manifest.6c2a3a1b645e179250c0682b926bc953.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee29b83d342b54b28d05cc45f8dff9d0",
+    "url": "/bryansomto/project/index.html"
+  },
+  {
+    "revision": "65fbf03786c95d41c40a",
+    "url": "/bryansomto/project/static/css/2.37c457eb.chunk.css"
+  },
+  {
+    "revision": "8cd3efb1aa7a94741ab9",
+    "url": "/bryansomto/project/static/css/main.950b1802.chunk.css"
+  },
+  {
+    "revision": "65fbf03786c95d41c40a",
+    "url": "/bryansomto/project/static/js/2.e4a1abf0.chunk.js"
+  },
+  {
+    "revision": "8cd3efb1aa7a94741ab9",
+    "url": "/bryansomto/project/static/js/main.290b0fad.chunk.js"
+  },
+  {
+    "revision": "da7ba445b9acf7add795",
+    "url": "/bryansomto/project/static/js/runtime-main.b1e5e03a.js"
+  }
+]);
