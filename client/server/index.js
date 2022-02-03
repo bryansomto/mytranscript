f0ed8e187b7cfbcdc391f0ad2eaf0824b7bc9b18
Somtochukwu Ike-Adinnu <bryansomto@gmail.com>
2022-02-03 15:29:58 +0100

    added employer api

--- client/server/index.js
+++ client/server/index.js
@@ -12,44 +12,33 @@ app.use(pino());
 const cors = require('cors');
 app.use(cors());
 
-const studentDetails = {'username': 'bryan', 'password': '0000'}
-const lecturerDetails = {'username': 'nzube', 'password': '1234'}
-
-// app.get('/api/greeting', (req, res) => {
-//     const name = req.query.name || 'World';
-//     res.setHeader('Content-Type', 'application/json');
-//     res.send(JSON.stringify({ greeting: `Hello ${name}!` }))
-// });
+const studentDetails = { 'username': 'bryan', 'password': '0000' }
+const lecturerDetails = { 'username': 'nzube', 'password': '1234' }
+const candidateDetails = { 'key': 'bryan0554' }
 
 app.post('/', (req, res) => {
-    // console.log(req.body);
     if (req.body.username == studentDetails.username && req.body.password == studentDetails.password) {
-        console.log('success');
-        // res.send(JSON.stringify({ greeting: `Hello ${req.body.username}!` }));
         res.send(JSON.stringify('student'));
     }
     else if (req.body.username == lecturerDetails.username && req.body.password == lecturerDetails.password) {
-        console.log('success');
-        // res.send(JSON.stringify({ greeting: `Hello ${req.body.username}!` }));
         res.send(JSON.stringify('lecturer'));
     }
     else {
-        console.log('fail');
+        res.send(JSON.stringify('fail'));
+    }
+});
+
+app.post('/api/employer', (req, res) => {
+    console.log(req.body.key);
+    if (req.body.key == candidateDetails.key) {
+        res.redirect('/student');
+    }
+    else {
+        res.send(JSON.stringify('fail'));
     }
 });
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
+
 });
-
-// app.use(express.static('routes'));
-
-// app.use(pino);
-// app.use(cors({ origin: ['http://localhost:3001', 'http://127.0.0.1:3001'], credentials: true }));
-// app.use(function (req, res, next) {
-//     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-//     res.header('Access-Control-Allow-Headers', true);
-//     res.header('Access-Control-Allow-Credentials', 'Content-Type');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     next();
-// });
